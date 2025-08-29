7d55d24e855e83969ce9550923c924fc6c64470c
soustruh <martin@struzsky.cz>
2025-08-29 19:30:31 +0200

    updated gitignore files & minor improvements 💅
    ruff formatting, organized imports, os.path → pathlib, …

--- {{cookiecutter.repository_folder_name}}/src/configuration.py
+++ {{cookiecutter.repository_folder_name}}/src/configuration.py
@@ -1,6 +1,7 @@
 import logging
-from pydantic import BaseModel, Field, ValidationError, field_validator
+
 from keboola.component.exceptions import UserException
+from pydantic import BaseModel, Field, ValidationError, field_validator
 
 
 class Configuration(BaseModel):
@@ -18,8 +19,8 @@ class Configuration(BaseModel):
         if self.debug:
             logging.debug("Component will run in Debug mode")
 
-    @field_validator('api_token')
+    @field_validator("api_token")
     def token_must_be_uppercase(cls, v):
         if not v.isupper():
-            raise UserException('API token must be uppercase')
+            raise UserException("API token must be uppercase")
         return v
